fc52088fc50448a5702d07e4f33789c75bdbfb2f
Baptiste <b.rousserie24@gmail.com>
2019-04-24 18:21:49 +0900

    Avoid measuring twice in nested for-loops

--- Jaccard.cpp
+++ Jaccard.cpp
@@ -9,9 +9,9 @@ Jaccard::Jaccard() {
     match();
 }
 
-Jaccard* Jaccard::singleton;
+Jaccard *Jaccard::singleton;
 
-Jaccard* Jaccard::get() {
+Jaccard *Jaccard::get() {
     if (!Jaccard::singleton) Jaccard::singleton = new Jaccard();
     return Jaccard::singleton;
 }
@@ -19,34 +19,28 @@ Jaccard* Jaccard::get() {
 void Jaccard::match() {
     enron entity = (*enron::get());
     int interval;
+    int i, j;
 
     ofstream output_file(get_enron_path().append("/Jaccard_Measures.txt"));
-    for(int i = 0; i < NB_MAILS; i++) cout << "\t" << i;
 
+    for (i = 0; i < NB_MAILS; i++) {
+        for (j = i + 1; j < NB_MAILS; j++) {
 
-    for(int i = 0; i < NB_MAILS; i++) {
+            const set<int> &setA = (*entity.get_mails())[i];
+            const set<int> &setB = (*entity.get_mails())[j];
 
-        output_file << i;
-        for(int j = 0; j < NB_MAILS; j++) {
+            interval = 0;
 
-            if (i != j) {
-                cout << i << " - " << j << "\n";
+            for (int A : setA) {
+                for (int B : setB) {
 
-                const set<int> &setA = (*entity.get_mails())[i];
-                const set<int> &setB = (*entity.get_mails())[j];
+                    if (A == B) interval++;
 
-                interval = 0;
-
-                for (int A : setA) {
-                    for (int B : setB) {
-
-                        if (A == B) interval++;
-
-                    }
                 }
-                output_file << "#" << i+1 << " - #" << j+1 << " "
-                    << (float)interval / (float)(setA.size() + setB.size()) << "\n";
             }
+            output_file << "#" << i + 1 << " - #" << j + 1 << " "
+                        << (float) interval / (float) (setA.size() + setB.size()) << "\n";
+
         }
     }
     output_file.close();
